554ccc6ef5bede1357ad31bb8aba206dfe2191e9
bsyso <bsyso@softserveinc.com>
2022-12-30 09:05:40 +0100

    Initial commit

---
+++ src/main/java/com/photo_app_discovery_service/PhotoAppDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.photo_app_discovery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppDiscoveryServiceApplication.class, args);
+    }
+
+}
